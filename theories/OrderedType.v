584ff7f600ddd346e35a1e12bab4cbba2b09f38f
Eddy Westbrook <eddywestbrook@gmail.com>
2016-02-24 10:03:32 -0800

    defined OTPair and context weakening; got as far as defining proper_fun, though the proof is not yet complete

--- theories/OrderedType.v
+++ theories/OrderedType.v
@@ -62,8 +62,8 @@ Program Definition OTProp : OrderedType :=
     ot_R := iff;
   |}.
 Next Obligation.
-constructor.
-{ intros. transitivity y; assumption. }
+  constructor.
+  { intros. transitivity y; assumption. }
 Qed.
 
 (* NOTE: the following definition requires everything above to be polymorphic *)
@@ -93,13 +93,13 @@ Program Definition OTArrow
               forall x y, validR (ot_R A) x y -> ot_R B (f x) (g y);
   |}.
 Next Obligation.
-constructor. intros f g h; intros. destruct H4; destruct H5.
-apply (semitransitivity (SemiTransitive:=B) _ (g y)).
-{ apply H; repeat split; assumption. }
-{ apply H0; repeat split; assumption. }
-{ apply H1; repeat split; assumption. }
-{ apply H2; repeat split; assumption. }
-{ apply H3; repeat split; assumption. }
+  constructor. intros f g h; intros. destruct H4; destruct H5.
+  apply (semitransitivity (SemiTransitive:=B) _ (g y)).
+  { apply H; repeat split; assumption. }
+  { apply H0; repeat split; assumption. }
+  { apply H1; repeat split; assumption. }
+  { apply H2; repeat split; assumption. }
+  { apply H3; repeat split; assumption. }
 Qed.
 
 (*
@@ -122,7 +122,7 @@ dependent version of OTContext, below. *)
 
 
 (***
- *** Terms-in-Context
+ *** Types-in-Context
  ***)
 
 Definition OTContext := list OrderedType.
@@ -137,7 +137,7 @@ Fixpoint ctx_elem_lt (ctx:OTContext) : relation (ctx_elem ctx) :=
   match ctx with
   | [] => fun _ _ => True
   | A::ctx' => fun e1 e2 =>
-                 validR (ot_R A) (fst e1) (fst e1) /\
+                 validR (ot_R A) (fst e1) (fst e2) /\
                  ctx_elem_lt ctx' (snd e1) (snd e2)
   end.
 
@@ -147,26 +147,106 @@ Program Definition OTCtxElem (ctx:OTContext) : OrderedType :=
     ot_R := ctx_elem_lt ctx;
   |}.
 Next Obligation.
-induction ctx; constructor.
-{ intro ce. apply I. }
-{ intro; intros; apply I. }
-{ intro ce; split.
-  + apply (PreOrder_Reflexive (PreOrder:=a) (fst ce)).
-  + apply (PreOrder_Reflexive (PreOrder:=IHctx) (snd ce)). }
-{ intros ce1 ce2 ce3; intros. destruct H; destruct H0. split.
-  + apply (PreOrder_Transitive (PreOrder:=a) _ (fst ce2)); assumption.
-  + apply (PreOrder_Transitive (PreOrder:=IHctx) _ (snd ce2)); assumption. }
+  induction ctx; constructor.
+  { intros; assumption. }
+  { repeat (intro foo; destruct foo). split.
+    - destruct H; destruct H3; destruct H5; destruct H11;
+        destruct H7; destruct H13.
+      repeat split; try assumption.
+      apply (@semitransitivity _ _ (ot_PreOrder a) _ (fst (o0, c0)));
+        try assumption.
+    - apply (semitransitivity _ (snd (o0, c0))); assumption.
+  }
 Qed.
 
-Definition ctx_nil : ctx_elem [] := tt.
-
-Definition ctx_cons {A:OrderedType} {ctx}
-            (x y:A) (r:ot_R A x y) (c:ctx_elem ctx) : ctx_elem (A::ctx) :=
-  existT _ x (existT2 _ _ y r c).
-
-
-
 Definition OTInCtx (ctx:OTContext) (A:OrderedType) : OrderedType :=
-  {|
-    ot_Type := 
-  |}.
+  OTArrow (OTCtxElem ctx) A.
+
+Definition OrderedTypeAndCtx : Type := OTContext * OrderedType.
+
+Definition OTElem_inCtx ctx A : Type := OTElem (OTInCtx ctx A).
+
+
+(***
+ *** Pairs of Related Elements
+ ***)
+
+(* Pairs of terms-in-context that are valid and related to each other *)
+Inductive OTPair ctx A : Type :=
+  mkOTPair (x1: ctx_elem ctx -> ot_Type A)
+           (x2: ctx_elem ctx -> ot_Type A)
+           (pf: validR (ot_R (OTInCtx ctx A)) x1 x2) :
+    OTPair ctx A.
+
+(* OTPairs in the empty context can be coerced to OTElems *)
+Program Definition ot_pair_to_elem A (p: OTPair [] A) : OTElem A :=
+  match p with
+  | mkOTPair _ _ x1 x2 pf => x1 tt
+  end.
+Next Obligation.
+  destruct pf; apply H. repeat split; apply I.
+Qed.
+
+(* FIXME: does not satisfy the uniform inheritance condition... *)
+Coercion ot_pair_to_elem : OTPair >-> OTElem.
+
+(* Weakening for OTContexts: captures that ctx2 is an extension of ctx1 by
+saying that any object in ctx1 can be translated to ctx2 *)
+Class OTCtxExtends ctx1 ctx2 : Type :=
+  weaken_context :
+    forall {A}, (ctx_elem ctx1 -> ot_Type A) ->
+                ctx_elem ctx2 -> ot_Type A.
+
+(* The rules for context extension, as type class instances *)
+Instance OTCtxExtends_base ctx : OTCtxExtends ctx ctx :=
+  fun {A} elem => elem.
+
+Instance OTCtxExtends_cons_both ctx1 ctx2
+         {ext:OTCtxExtends ctx1 ctx2} {B}
+  : OTCtxExtends (B::ctx1) (B::ctx2) :=
+  fun {A} elem ctx_elem =>
+    let (celem1, ctx_elem') := ctx_elem in
+    (ext _ (fun ctx_elem'' => elem (celem1, ctx_elem''))) ctx_elem'.
+
+Instance OTCtxExtends_cons_right ctx1 ctx2
+         {ext:OTCtxExtends ctx1 ctx2} {B}
+  : OTCtxExtends ctx1 (B::ctx2) :=
+  fun {A} elem ctx_elem =>
+    let (_, ctx_elem') := ctx_elem in
+    (ext _ elem) ctx_elem'.
+
+
+(***
+ *** Builders for Proper Terms using OTPair
+ ***)
+
+Program Definition proper_var ctx A : OTPair (A::ctx) A :=
+  mkOTPair (A::ctx) A
+           (fun ctx_elem =>
+              let (celem_A,_) := ctx_elem in celem_A)
+           (fun ctx_elem =>
+              let (celem_A,_) := ctx_elem in celem_A)
+           _.
+Next Obligation.
+  repeat split;
+    intros x y; destruct x; destruct y; unfold fst; unfold snd;
+      intros; repeat (destruct H); repeat (destruct H0);
+        destruct H2; destruct H3; destruct H5; destruct H3; destruct H9;
+          assumption.
+Qed.
+
+Program Definition proper_fun {ctx} {A} {B}
+        (f: (forall {ctx'} {ext: OTCtxExtends (A::ctx) ctx'}, OTPair ctx' A) ->
+            OTPair (A::ctx) B) :
+  OTPair ctx (OTArrow A B) :=
+  let (y1,y2,pf_y) :=
+      f (fun {ctx'} {ext} =>
+           let (x1,x2,pf_x) := proper_var ctx A in
+           mkOTPair _ _ (weaken_context x1)
+                    (weaken_context x2)
+                    _)
+  in
+  mkOTPair _ _ (fun ctx_elem x => y1 (x,ctx_elem))
+           (fun ctx_elem x => y2 (x,ctx_elem)) _.
+Next Obligation.
+  repeat split; intros.
