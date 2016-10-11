7760fa64184e54edf9da1980f4ea73b449dbb42a
Eddy Westbrook <westbrook@galois.com>
2016-10-11 12:19:33 -0700

    got what I think is the right approach to automation working in SemiPreOrder.v and Monad.v

--- theories/Monad.v
+++ theories/Monad.v
@@ -7,22 +7,21 @@ Require Export PredMonad.SemiPreOrder.
  *** The monad typeclass
  ***)
 
-Polymorphic Class MonadOps (M : Type -> Type) : Type :=
+Class MonadOps (M : Type -> Type) : Type :=
   { returnM : forall {A : Type}, A -> M A;
     bindM : forall {A B : Type}, M A -> (A -> M B) -> M B;
     lrM :> forall {A : Type} `{LR_Op A}, LR_Op (M A) }.
 
-Polymorphic Class Monad (M : Type -> Type) {MonadOps:MonadOps M} : Prop :=
+Class Monad (M : Type -> Type) {MonadOps:MonadOps M} : Prop :=
   {
     monad_LR :> forall `{LR}, LR (M A);
 
     monad_proper_return :>
-      forall {A R} `{@LR A R}, Proper (lr_leq ==> lr_leq) returnM;
+      forall {A R} `{@LR A R}, Proper lr_leq returnM;
 
     monad_proper_bind :>
       forall {A B RA RB} `{@LR A RA} `{@LR B RB},
-        Proper (lr_leq ==> lr_leq ==> lr_leq)
-               (bindM (A:=A) (B:=B));
+        Proper lr_leq (bindM (A:=A) (B:=B));
 
     monad_proper_lrM :>
       forall {A},
@@ -30,7 +29,7 @@ Polymorphic Class Monad (M : Type -> Type) {MonadOps:MonadOps M} : Prop :=
 
     monad_return_bind :
       forall {A B RA RB} `{@LR A RA} `{@LR B RB} x (f:A -> M B)
-             `{Proper _ lr_leq x} `{Proper _ (lr_leq ==> lr_leq) f},
+             `{Proper _ lr_leq x} `{Proper _ lr_leq f},
         bindM (returnM x) f ~~ f x;
 
     monad_bind_return :
@@ -41,97 +40,31 @@ Polymorphic Class Monad (M : Type -> Type) {MonadOps:MonadOps M} : Prop :=
       forall {A B C RA RB RC}
              `{@LR A RA} `{@LR B RB} `{@LR C RC}
              m (f:A -> M B) (g:B -> M C)
-             `{Proper _ lr_leq m} `{Proper _ (lr_leq ==> lr_leq) f}
-             `{Proper _ (lr_leq ==> lr_leq) g},
+             `{Proper _ lr_leq m} `{Proper _ lr_leq f}
+             `{Proper _ lr_leq g},
         bindM (bindM m f) g ~~ bindM m (fun x => bindM (f x) g);
 
   }.
 
 
-(***
- *** Helper theorems / utilities about monads
- ***)
-
 (* Helpful bind notation *)
 Notation "'do' x <- m1 ; m2" :=
   (bindM m1 (fun x => m2)) (at level 60, right associativity).
-
-Instance Monad_lr_leq_SemiPreOrder `{Monad} `{LR} : SemiPreOrder (lr_leq (A:=M A)).
-Proof.
-  auto with typeclass_instances.
-Qed.
-
-Instance Monad_lr_eq_SemiPreOrder `{Monad} `{LR} : SemiPreOrder (lr_eq (A:=M A)).
-Proof.
-  apply PER_SemiPreOrder.
-Qed.
-
-
-(*
-
-(* FIXME: Equivalence and friends are not polymorphic... *)
-Instance equalsM_Equivalence `{Monad} `{Equals} : Equivalence (equalsM (A:=A)).
-  apply monad_equalsM; assumption.
-Qed.
-
-(* FIXME: why is this not automatic in the proof of bind_fun_equalsM?!? *)
-Instance equalsM_Reflexive `{Monad} `{Equals} : Reflexive (equalsM (A:=A)).
-  auto with typeclass_instances.
-Qed.
-
-Instance equalsM_Symmetric `{Monad} `{Equals} : Symmetric (equalsM (A:=A)).
-  auto with typeclass_instances.
-Qed.
-
-Instance equalsM_Transitive `{Monad} `{Equals} : Transitive (equalsM (A:=A)).
-  auto with typeclass_instances.
-Qed.
-
-Add Parametric Relation `{Monad} `{Equals} : (M A) equalsM
-    reflexivity proved by equalsM_Reflexive
-    symmetry proved by equalsM_Symmetric
-    transitivity proved by equalsM_Transitive
-as equalsM_Equivalence2.
-
-Instance equalsM_equals_iff_Proper `{Monad} `{Equals} :
-  Proper (equals ==> equals ==> iff) equalsM.
-intros m11 m12 e1 m21 m22 e2; split; intros.
-transitivity m11; [ symmetry; assumption | transitivity m21; assumption ].
-transitivity m12; [ assumption |
-                    transitivity m22; try assumption; symmetry; assumption ].
-Qed.
-
-Instance equalsM_equals_impl_Proper `{Monad} `{Equals} :
-  Proper (equals ==> equals ==> Basics.flip Basics.impl) equalsM.
-intros m11 m12 e1 m21 m22 e2 e12.
-transitivity m12; [ assumption |
-                    transitivity m22; try assumption; symmetry; assumption ].
-Qed.
-
-Lemma bind_fun_equalsM `{Monad} {A B:Type}
-      `{Equals A} `{Equals B} m (f1 f2:A -> M B) :
-  (forall x, f1 x == f2 x) -> bindM m f1 == bindM m f2.
-Proof.
-  intros e; apply (monad_proper_bind A B (H:=Eq_Equals A) m m).
-  (* FIXME: why does the reflexivity tactic not work? *)
-  apply (equalsM_Reflexive (H0:=Eq_Equals A)).
-  intros x y exy; rewrite exy; apply e.
-Qed.
-
-*)
 
 
 (***
  *** The Identity Monad
  ***)
 
-Polymorphic Definition Identity (X:Type) := X.
-Polymorphic Instance IdMonad_MonadOps : MonadOps Identity :=
+Definition Identity (X:Type) := X.
+
+Instance IdMonad_MonadOps : MonadOps Identity :=
   { returnM := fun A x => x;
     bindM := fun A B m f => f m;
-    lrM := fun A R => lr_leq }.
+    lrM := fun A R => R }.
 
-Polymorphic Instance IdMonad : Monad Identity.
-constructor; intros; unfold returnM, bindM, lrM, IdMonad_MonadOps; auto with LR.
-repeat prove_proper_fun. elim_lr_fun.
+Instance IdMonad : Monad Identity.
+constructor; intros; unfold returnM, bindM, lrM, IdMonad_MonadOps;
+  try assumption; try prove_lr; prove_lr_proper.
+intros R1 R2 sub; assumption.
 Qed.
--- theories/SemiPreOrder.v
+++ theories/SemiPreOrder.v
@@ -2,7 +2,7 @@
  *** Semi Pre-Orders
  ***)
 
-Require Export Coq.Program.Tactics.
+Require Export Coq.Program.Basics.
 Require Export Coq.Setoids.Setoid.
 Require Export Coq.Classes.Morphisms.
 Require Export Coq.Arith.Arith_base.
@@ -91,20 +91,29 @@ Proof. apply PER_SemiPreOrder. Qed.
 
 
 (***
- *** Automation for proving terms are related by a logical relation
+ *** Some tactics and Proper instances for logical relations
  ***)
-
-Create HintDb LR.
 
 (* Tactic to apply semi-reflexivity to goals x <~ x if there is an assumption
 with the form x <~ y or y <~ x *)
-Ltac apply_semi_reflexivity :=
-  lazymatch goal with
+Ltac assumption_semi_refl :=
+  match goal with
   | H : ?x <~ ?y |- ?x <~ ?x => apply (semi_reflexivity_l _ _ H)
   | H : ?y <~ ?x |- ?x <~ ?x => apply (semi_reflexivity_r _ _ H)
+  | H : ?x ~~ ?y |- ?x <~ ?x => destruct H; assumption_semi_refl
+  | H : ?y ~~ ?x |- ?x <~ ?x => destruct H; assumption_semi_refl
+  | H : ?x ~~ ?y |- ?x ~~ ?x => apply (semi_reflexivity_l _ _ H)
+  | H : ?y ~~ ?x |- ?x ~~ ?x => apply (semi_reflexivity_r _ _ H)
+  (*
+  | |- ?f ?z <~ ?f ?z =>
+    apply semi_reflexivity_apply_Proper_leq; semi_reflexivity
+  | |- ?f ?z ~~ ?f ?z =>
+    apply semi_reflexivity_apply_Proper_eq; semi_reflexivity
+  | |- ?R ?f ?f =>
+    change (Proper R f); eauto with typeclass_instances
+   *)
   end.
 
-Hint Extern 1 (?x <~ ?x) => apply_semi_reflexivity : LR.
 
 (* Tactic to apply transitivity to goals x <~ z if there is an assumption with
 the form x <~ y or y <~ z *)
@@ -114,61 +123,245 @@ Ltac apply_transitivity :=
   | H : ?y <~ ?z |- ?x <~ ?z => apply (transitivity (y:=?y)); try assumption
   end.
 
-Hint Extern 1 (?x <~ ?z) => apply_transitivity : LR.
 
-(* Hint to prove proper-ness of functions *)
-Ltac prove_proper_fun :=
-  let x := fresh "x" in
-  let y := fresh "y" in
-  let H := fresh "H" in
-  intros x y H.
+(* Rewrite using an lr_leq or lr_eq assumption *)
+Ltac rewrite_assumption :=
+  match goal with
+  | H : ?x <~ ?y |- ?u <~ ?v => rewrite <- H
+  | H : ?x ~~ ?y |- ?u ~~ ?v => rewrite H
+  end.
 
-Hint Extern 1 (Proper (?R1 ==> ?R2) ?f) => prove_proper_fun : LR.
 
-Hint Extern 1 (?x ~~ ?y) => split : LR.
+(* FIXME HERE: not sure if these Proper instances are needed... *)
+
+Instance lr_leq_Proper `{LR} : Proper (flip lr_leq ==> lr_leq ==> impl) lr_leq.
+Proof.
+  intros x1 y1 R1 x2 y2 R2 R12. rewrite <- R1. rewrite <- R2. assumption.
+Qed.
+
+Instance lr_leq_Proper_flip `{LR} : Proper (lr_leq ==> flip lr_leq ==> flip impl) lr_leq.
+Proof.
+  intros x1 y1 R1 x2 y2 R2 R12. rewrite R1. rewrite R2. assumption.
+Qed.
+
+Instance lr_leq_Proper_proj1 `{LR} x : Proper (lr_leq ==> impl) (lr_leq x).
+Proof.
+  intros y1 y2 Ry. rewrite Ry. reflexivity.
+Qed.
+
+Instance lr_leq_Proper_proj1_flip `{LR} x :
+  Proper (lr_leq --> flip impl) (lr_leq x).
+Proof.
+  intros y1 y2 Ry. rewrite Ry. reflexivity.
+Qed.
+
+Instance lr_eq_Proper `{LR} : Proper (lr_eq ==> lr_eq ==> iff) lr_eq.
+Proof.
+  intros x1 y1 R1 x2 y2 R2; split; intro Rx.
+  + rewrite <- R1. rewrite <- R2. assumption.
+  + rewrite R1. rewrite R2. assumption.
+Qed.
+
+
+(* FIXME HERE: not sure if these subrelations are used... *)
+
+(* lr_eq is a sub-relation of lr_leq *)
+Instance subrelation_lr_eq_lr_leq `{LR} : subrelation lr_eq lr_leq.
+Proof.
+  intros x y Rxy; destruct Rxy; assumption.
+Qed.
+
+(* lr_eq is a sub-relation of the inverse of lr_leq *)
+Instance subrelation_lr_eq_flip_lr_leq `{LR} : subrelation lr_eq (flip lr_leq).
+Proof.
+  intros x y Rxy; destruct Rxy; assumption.
+Qed.
+
+
+(* FIXME HERE: not sure if these Proper lemmas are used... *)
+
+(* Any function that is Proper w.r.t. (lr_leq ==> lr_leq) also Proper
+w.r.t. (lr_eq ==> lr_eq) *)
+Instance Proper_lr_leq_lr_eq_unary {A B} `{LR A} `{LR B} (f:A -> B)
+         `{P:Proper _ (lr_leq ==> lr_leq) f} : Proper (lr_eq ==> lr_eq) f.
+Proof.
+  intros x y Rxy; destruct Rxy; split; apply P; assumption.
+Qed.
+
+(* Similar to the above, but for binary functions *)
+Instance Proper_lr_leq_lr_eq_binary {A B C} `{LR A} `{LR B} `{LR C}
+         (f:A -> B -> C)
+         `{P:Proper _ (lr_leq ==> lr_leq ==> lr_leq) f} :
+  Proper (lr_eq ==> lr_eq ==> lr_eq) f.
+Proof.
+  intros x1 y1 Rxy1 x2 y2 Rxy2; destruct Rxy1; destruct Rxy2;
+    split; apply P; assumption.
+Qed.
+
+(* Similar to the above, but for trinary functions *)
+Instance Proper_lr_leq_lr_eq_trinary {A B C D} `{LR A} `{LR B} `{LR C} `{LR D}
+         (f:A -> B -> C -> D)
+         `{P:Proper _ (lr_leq ==> lr_leq ==> lr_leq ==> lr_leq) f} :
+  Proper (lr_eq ==> lr_eq ==> lr_eq ==> lr_eq) f.
+Proof.
+  intros x1 y1 Rxy1 x2 y2 Rxy2 x3 y3 Rxy3;
+    destruct Rxy1; destruct Rxy2; destruct Rxy3;
+    split; apply P; assumption.
+Qed.
 
 
 (***
- *** Pre-Defined Logical Relations
+ *** Logical Relation for Functions and Pairs
  ***)
-
-(* The LR for pairs is just the pointwise relation on the components *)
-Instance LR_Op_pair {A B} `{LR_Op A} `{LR_Op B} : LR_Op (A * B) :=
-  fun p1 p2 =>
-    lr_leq (fst p1) (fst p2) /\ lr_leq (snd p1) (snd p2).
-
-Instance LR_pair {A B} `{LR A} `{LR B} : LR (A * B).
-Proof.
-  constructor; constructor.
-  constructor; intros p1 p2 R12; split; destruct R12; auto with LR.
-  intros p1 p2 p3 R12 R23; destruct R12; destruct R23; split; auto with LR.
-Qed.
 
 (* The LR for functions is the pointwise relation on all the outputs, restricted
 so to only relate proper functions *)
-Instance LR_Op_fun {A B} `{LR_Op A} `{LR_Op B} : LR_Op (A -> B) :=
-  fun f g =>
-    forall x y, x <~ y -> f x <~ f y /\ g x <~ g y /\ f x <~ g y.
+Record LRFun {A B} `{LR_Op A} `{LR_Op B} (f g : A -> B) : Prop :=
+  {
+    LRFun_Proper_l : Proper (lr_leq ==> lr_leq) f;
+    LRFun_Proper_r : Proper (lr_leq ==> lr_leq) g;
+    apply_lr_leq : forall x y, x <~ y -> f x <~ g y
+  }.
 
+(* Make LRFun itself an instance of LR_Op *)
+Instance LR_Op_fun {A B} `{LR_Op A} `{LR_Op B} : LR_Op (A -> B) := LRFun.
+
+(* LR instance for functions *)
 Instance LR_fun {A B} `{LR A} `{LR B} : LR (A -> B).
 Proof.
   constructor; constructor.
-  { constructor; intros f g Rfg x y Rxy;
-      destruct (Rfg x y) as [ Rfg1 Rfg2 ]; try assumption;
-        destruct Rfg2 as [ Rfg2 Rfg3 ]; repeat split; assumption. }
-  { intros f g h Rfg Rgh x y Rxy; repeat split.
-    + destruct (Rfg x y); assumption.
-    + destruct (Rgh x y) as [ Rgh1 Rgh2 ]; try assumption; destruct Rgh2; assumption.
-    + transitivity (g x).
-      - apply Rfg; apply (semi_reflexivity _ y); left; assumption.
-      - apply Rgh; assumption. 
-  }
+  { constructor; intros f g Rfg; destruct Rfg as [ Pf Pg Rfg ]; constructor;
+      intros x y Rxy; first [ apply Pf | apply Pg | apply Rfg ]; assumption. }
+  { intros f g h Rfg Rgh; destruct Rfg as [ Pf Pg Rfg ];
+      destruct Rgh as [ Pg' Ph Rgh ]; constructor; intros x y Rxy.
+    - apply Pf; assumption.
+    - apply Ph; assumption.
+    - transitivity (g x).
+      + apply Rfg; assumption_semi_refl.
+      + apply Rgh; assumption. }
 Qed.
 
-(* Tactic for eliminating a relation on functions *)
-Ltac elim_lr_fun :=
-  lazymatch goal with
-  | H : ?f <~ ?g |- ?f ?x <~ ?g ?y => apply H; try assumption
-  end.
+(* Elimination principle for proving ~~ from functions *)
+Lemma apply_lr_eq {A B} `{LR_Op A} `{LR_Op B} (f g : A -> B) :
+  f ~~ g -> forall x y, x ~~ y -> f x ~~ g y.
+  intros Rfg x y Rxy; destruct Rfg as [ Rfg1 Rfg2 ]; destruct Rxy; split;
+    [ apply Rfg1 | apply Rfg2 ]; assumption.
+Qed.
 
-Hint Extern 1 (?f ?x <~ ?g ?y) => elim_lr_fun : LR.
+
+(* The LR for pairs is just the pointwise relation on the components *)
+Class LRPair {A B} `{LR_Op A} `{LR_Op B} (p1 p2 : A * B) : Prop :=
+  { LRPair_fst :> fst p1 <~ fst p2;
+    LRPair_snd :> snd p1 <~ snd p2
+  }.
+
+(* Make LRPair itself an instance of LR_Op *)
+Instance LR_Op_pair {A B} `{LR_Op A} `{LR_Op B} : LR_Op (A * B) := LRPair.
+
+(* Prove the LR instance for LRPair *)
+Instance LR_pair {A B} `{LR A} `{LR B} : LR (A * B).
+Proof.
+  constructor; constructor.
+  { constructor; intros p1 p2 R12; destruct R12; split; assumption_semi_refl. }
+  { intros p1 p2 p3 R12 R23; destruct R12; destruct R23; split;
+      [ transitivity (fst p2) | transitivity (snd p2) ]; assumption. }
+Qed.
+
+(* fst is a Proper morphism *)
+Instance Proper_LRPair_fst {A B} `{LR_Op A} `{LR_Op B} :
+  Proper lr_leq (fst : (A*B) -> A).
+Proof.
+  split; intros p1 p2 Rp; destruct Rp; assumption.
+Qed.
+
+(* snd is a Proper morphism *)
+Instance Proper_LRPair_snd {A B} `{LR_Op A} `{LR_Op B} :
+  Proper lr_leq (snd : (A*B) -> B).
+Proof.
+  split; intros p1 p2 Rp; destruct Rp; assumption.
+Qed.
+
+(* pair is a Proper morphism *)
+Instance Proper_LRPair_pair {A B} `{LR A} `{LR B} :
+  Proper lr_leq (pair : A -> B -> A*B).
+Proof.
+  split; intros x1 y1 Rxy1; split; intros x2 y2 Rxy2; split; unfold fst, snd;
+    try assumption; try assumption_semi_refl.
+Qed.
+
+
+(***
+ *** Automation
+ ***)
+
+(* Tactic to prove things like (f x y) <~ (f' x' y') and (f x y) ~~ (f' x' y')
+ by reducing them to f <~ f', x <~ x', and y <~ y' (or similar with ~~ in place
+ of <~).  This is similar to (and the code is adapted from) the f_equiv tactic
+ in Coq.Classes.Morphisms, except that we do not use rewriting or reflexivity
+ because lr_leq and lr_eq are not necessarily reflexive. *)
+Ltac prove_lr :=
+  match goal with
+  | |- (?f _) <~ (?g _) => apply apply_lr_leq; [ change (f <~ g) | ]; prove_lr
+  | |- (?f _) ~~ (?g _) => apply apply_lr_eq; prove_lr
+  | |- ?f <~ ?g =>
+    first [ change (Proper lr_leq f); solve [ eauto with typeclass_instances ]
+          | change (f <~ f); assumption_semi_refl
+          | assumption
+          | idtac ]
+  | |- ?f ~~ ?g =>
+    first [ change (Proper lr_eq f); solve [ eauto with typeclass_instances ]
+          | split; change (Proper lr_leq f);
+            solve [ eauto with typeclass_instances ]
+          | change (f ~~ f); assumption_semi_refl
+          | assumption
+          | idtac ]
+ end.
+
+
+(* To prove a function is Proper w.r.t. lr_leq, we only need to prove it is
+Proper w.r.t. (lr_leq ==> lr_leq). Note that we do *not* make this an Instance,
+since this is only for use in the lr_prove_proper tactic, below. *)
+Lemma fun_Proper_lr_leq {A B} `{LR A} `{LR B} (f: A -> B) :
+  Proper (lr_leq ==> lr_leq) f -> Proper lr_leq f.
+  intro Rf; constructor; assumption.
+Qed.
+
+(* Helper lemma for the lr_prove_proper tactic *)
+Lemma fun_Proper_lr_leq_adjoint {A B C} `{LR A} `{LR B} `{LR C} (f: A -> B -> C) :
+  Proper (lr_leq ==> lr_leq) (fun p => let (x,y) := p : A*B in f x y) ->
+  Proper (lr_leq ==> lr_leq) f.
+  intros Pf x1 y1 Rxy1; constructor; intros x2 y2 Rxy2;
+    (lazymatch goal with
+     | |- f ?x1 ?x2 <~ f ?y1 ?y2 => apply (Pf (x1,x2) (y1,y2))
+     end); split; prove_lr.
+Qed.
+
+(* Helper lemma for the lr_prove_proper tactic *)
+Lemma fun_Proper_arrow_pair_commute {A B C D} `{LR A} `{LR B} `{LR C} RD
+      (f: A * (B * C) -> D) :
+  Proper (lr_leq ==> RD) (fun (p:(A * B) * C) =>
+                            match p with ((x,y),z) => f (x,(y,z)) end) ->
+  Proper (lr_leq ==> RD) f.
+  intros Pf p1 p2 Rp; destruct p1 as [ x1 p1 ]; destruct p1 as [ y1 z1 ];
+    destruct p2 as [ x2 p2 ]; destruct p2 as [ y2 z2 ];
+      destruct Rp as [ Rx Rp ]; destruct Rp.
+  apply (Pf (x1, y1, z1) (x2, y2, z2)).
+  repeat split; unfold fst, snd in * |- *; assumption.
+Qed.
+
+(* Helper lemma for the lr_prove_proper tactic *)
+Lemma fun_Proper_arrow_adjoint {A B C} `{LR A} `{LR B} RC (f: A * B -> C) :
+  Proper (lr_leq ==> (lr_leq ==> RC)) (fun x y => f (x,y)) ->
+  Proper (lr_leq ==> RC) f.
+  intros Pf p1 p2 Rp; destruct p1; destruct p2; destruct Rp; apply Pf;
+    unfold fst, snd in * |- *; assumption.
+Qed.
+
+(* Tactic to prove Proper lr_leq f from Proper (lr_leq ==> ... ==> lr_leq) f *)
+Ltac prove_lr_proper :=
+  repeat (first [ solve [ eauto with typeclass_instances ]
+                | progress (apply fun_Proper_lr_leq)
+                | apply fun_Proper_lr_leq_adjoint
+                | apply fun_Proper_arrow_pair_commute
+                | apply fun_Proper_arrow_adjoint
+                | repeat (intro; intros); prove_lr ]).
