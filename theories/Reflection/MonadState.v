8ce07c61beddd94ad844155acd7aefca69fa4683
Eddy Westbrook <westbrook@galois.com>
2017-07-25 09:42:47 -0700

    got the proof of the Monad instance for the state monad to work, and started writing the state monad laws as OExpr rewrites; these need some more support in the UnQuotesTo mechanism to make them work, so I put comments in for what is needed

--- theories/Reflection/MonadState.v
+++ theories/Reflection/MonadState.v
@@ -17,29 +17,56 @@ Class MonadState M `{MonadStateOps M} : Prop :=
     monad_state_monad :> Monad M;
 
     monad_state_get :
-      forall A `{OType A} (m : M A _ _),
-        bindM @o@ getM @o@ (ofun (fun _ => m)) =o= m;
+      forall A `{OType A} (m : M A _ _) prp,
+        bindM @o@ getM @o@ (ofun (fun _ => m) (prp:=prp)) =o= m;
 
     monad_state_get_put :
-      forall A `{OType A} (f : St -o> unit -o> M A _ _),
+      forall A `{OType A} (f : St -o> unit -o> M A _ _) prp1 prp2,
         bindM @o@ getM @o@
-              (ofun (fun s => bindM @o@ (putM @o@ s) @o@ (f @o@ s)))
+              (ofun (fun s => bindM @o@ (putM @o@ s) @o@ (f @o@ s)) (prp:=prp1))
         =o= bindM @o@ getM @o@
-                  (ofun (fun s => f @o@ s @o@ tt));
+                  (ofun (fun s => f @o@ s @o@ tt) (prp:=prp2));
 
     monad_state_put_get :
-      forall A `{OType A} s (f : unit -o> St -o> M A _ _),
+      forall A `{OType A} s (f : unit -o> St -o> M A _ _) prp1 prp2,
         bindM @o@ (putM @o@ s) @o@
-              (ofun (fun u => bindM @o@ getM @o@ (f @o@ u)))
+              (ofun (fun u => bindM @o@ getM @o@ (f @o@ u)) (prp:=prp1))
         =o= bindM @o@ (putM @o@ s) @o@
-                  (ofun (fun u => f @o@ u @o@ s)) ;
+                  (ofun (fun u => f @o@ u @o@ s) (prp:=prp2)) ;
 
     monad_state_put_put :
-      forall A `{OType A} s1 s2 (f : unit -o> unit -o> M A _ _),
+      forall A `{OType A} s1 s2 (f : unit -o> unit -o> M A _ _) prp,
         bindM @o@ (putM @o@ s1) @o@
-              (ofun (fun u => bindM @o@ (putM @o@ s2) @o@ (f @o@ u)))
+              (ofun (fun u => bindM @o@ (putM @o@ s2) @o@ (f @o@ u)) (prp:=prp))
         =o= bindM @o@ (putM @o@ s2) @o@ (f @o@ tt)
   }.
+
+
+(***
+ *** The State Monad Laws for OExprs
+ ***)
+
+(*
+FIXME HERE NOW: cannot match weakenOExpr on the LHS; instead, we need a
+Strengthens typeclass for lifting m out of its context
+
+FIXME HERE NOW: also need a rule for unquoting 
+
+Lemma monad_state_get_OExpr
+      {ctx} `{ValidCtx ctx} `{MonadState} {A} `{OType A}
+      m {validM: @ValidOExpr ctx (M A _ _) _ m} :
+  App (App (Embed bindM) (Embed getM)) (Lam (weakenOExpr 0 _ m)) =e= m.
+Proof.
+  apply unquote_eq. intro. simpl. apply monad_state_get.
+Qed.
+
+Lemma monad_state_get_put_OExpr
+      {ctx} `{ValidCtx ctx} `{MonadState} {A} `{OType A}
+      f {validF: @ValidOExpr _ (St -o> unit -o> M A _ _) _ f} :
+  App (App (Embed bindM) (Embed getM))
+      (Lam (App (App (Embed bindM) (App (Embed putM) (Var OVar_0)))
+                (Lam (App (App f )))))
+*)
 
 
 (***
@@ -70,31 +97,14 @@ Instance StateT_MonadOps St `{OType St} M `{MonadOps M} : MonadOps (StateT St M)
                               f @o@ (osnd @o@ s_x) @o@ (ofst @o@ s_x))))
   }.
 
-Typeclasses Opaque celem_head celem_rest.
+(* Typeclasses Opaque celem_head celem_rest. *)
 
 (* The Monad instance for StateT *)
 Instance StateT_Monad St `{OType St} M `{Monad M} : Monad (StateT St M).
 Proof.
   constructor; intros.
-  - unfold bindM, returnM, StateT_MonadOps, StateT.
-    simpl. oquote. oexpr_simpl. simpl. try reflexivity. assumption.
-  - unfold bindM, returnM, StateT_MonadOps, StateT.
-    simpl. oquote. oexpr_simpl. try reflexivity. assumption.
-  - unfold bindM, returnM, StateT_MonadOps, StateT.
-    simpl.
-    oquote. oexpr_simpl. unfold weakenOExpr, weakenOVar.
-    repeat first [ reflexivity | apply Proper_Lam_eq
-                   | apply Proper_App_eq | simpl; reflexivity ].
-    simpl.
-
-reflexivity.
-apply (Proper_Lam_eq _ _).
-
-    simpl. reflexivity.
-Set Printing All. idtac. simpl.
-    reflexivity.
-    apply Proper_Lam_eq. apply Proper_App_eq; try reflexivity.
-
-reflexivity.
-
-try reflexivity. assumption.
+  Typeclasses eauto := debug.
+  - osimpl.
+  - osimpl.
+  - osimpl.
+Qed.
